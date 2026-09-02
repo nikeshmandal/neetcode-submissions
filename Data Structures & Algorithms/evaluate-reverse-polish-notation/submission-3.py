56c0e1e3bc93910810c273ebeab54fa1e212012f
Nikesh Kumar Mandal <118096553+nikeshmandal@users.noreply.github.com>
2026-09-02 23:22:09 +0530

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stacka=[]
+
+        for c in range(len(tokens)):
+            if (tokens[c] == '+'):
+                b=stacka.pop()
+                d=stacka.pop()
+                stacka.append(d+b)
+            elif (tokens[c] == '-'):
+                b=stacka.pop()
+                d=stacka.pop()
+                stacka.append(d-b)
+            elif (tokens[c] == '*'):
+                b=stacka.pop()
+                d=stacka.pop()
+                stacka.append(d*b)
+            elif (tokens[c] == '/'):
+                b=stacka.pop()
+                d=stacka.pop()
+                stacka.append(int(d/b))
+            
+            else:
+                stacka.append(int(tokens[c]))
+
+        return int(stacka[0])
+
+
